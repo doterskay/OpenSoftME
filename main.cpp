e1fb6c7a607b66baabb7d3cc86df13943413ea71
Dmitrei <zxc524941@gmail.com>
2026-05-28 23:49:46 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ int main(){
     time_t now = time(0);
     tm* local = localtime(&now);
     
+    string months[]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
 
     cout << " Day end Time : ";
-    cout << local -> tm_mday << ".";
-    cout << local -> tm_mon << ".";
+    cout << local -> tm_mday << ". ";
+    cout << months[local -> tm_mon]<<". ";
     cout << local -> tm_year + 1900<< " ";
     cout << local-> tm_hour << ":";
     cout << local-> tm_min << ":";
