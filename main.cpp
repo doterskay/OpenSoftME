7c84b36f5255cff6381647f9d969003b21055f76
Dmitrei <zxc524941@gmail.com>
2026-05-29 21:07:15 +0500

    add open Youtube

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main(){
         cout << "  1 - Показать время\n";
         cout << "  2 - Показать дату\n";
         cout << "  3 - Показать всё\n";
+        cout << "  4 - Открыть YouTube в браузере      \n";
         cout << "  0 - Выйти\n";
 
         
@@ -40,6 +41,10 @@ int main(){
             << local->tm_min << ":" << local->tm_sec << "\n";
             cout << local->tm_mday << " " << months[local-> tm_mon] << " "<< local->tm_year + 1900;
 
+        }
+        if (choice == 4) {
+            cout << "Open Youtube im Browser.....\n";
+            system("start https://www.youtube.com");
         }
       
         
