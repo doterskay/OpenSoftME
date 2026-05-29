6e689fa8d366da958713da0b0b0d30f0fbfbf4c2
Dmitrei <zxc524941@gmail.com>
2026-05-30 00:03:39 +0500

    Update version 1.1 --> 1.2

--- main.cpp
+++ main.cpp
@@ -17,17 +17,14 @@ int main(){
         SetConsoleOutputCP(CP_UTF8);
 
         cout << "\n┌─────────────────────────────────┐\n";
-        cout << "│         SYSTEM TIME v1.1        │\n";
+        cout << "│         SYSTEM TIME v1.2        │\n";
         cout << "├─────────────────────────────────┤\n";
-        cout << "  1 - Показать время\n";
-        cout << "  2 - Показать дату\n";
-        cout << "  3 - Показать всё\n";
-        cout << "  4 - Открыть YouTube в браузере      \n";
-        cout << "  5 - VK в браузере      \n";
-        cout << "  6 - Spotify в браузере      \n";
+        cout << "  1 - Показать всё\n";
+        cout << "  2 - Открыть YouTube в браузере      \n";
+        cout << "  3 - VK в браузере      \n";
+        cout << "  4 - Spotify в браузере      \n";
+        cout << "  5 - Ping in DNS 8.8.8.8      \n";
         cout << "  0 - Выйти\n";
-
-        
         cout << "└─────────────────────────────────┘\n";
         cin >> choice;
         
@@ -35,31 +32,30 @@ int main(){
         tm* local = localtime(&now);
 
 
-        if (choice == 1) {
-            cout << "\n Time: "<< local-> tm_hour<< ":"
-            << local->tm_min << ":" << local->tm_sec << "\n";
-        }
+        
 
-        else if(choice == 2) {
-             cout << local->tm_mday << " " << months[local-> tm_mon] << " "<< local->tm_year + 1900;
-        }
-        else if (choice == 3) { 
+        
+         if (choice == 1) { 
             cout << "\nTime: "<< local-> tm_hour<< ":"
             << local->tm_min << ":" << local->tm_sec << "\n";
             cout << local->tm_mday << " " << months[local-> tm_mon] << " "<< local->tm_year + 1900;
 
         }
-        else if (choice == 4) {
+        else if (choice == 2) {
             cout << "Open Youtube im Browser.....\n";
             system("start https://www.youtube.com");
         }
-        else if( choice == 5) { 
+        else if( choice == 3) { 
             cout<< "Open Vk music... \n";
             system("start https://vk.com "); 
         }
-        else if ( choice == 6) { 
+        else if ( choice == 4) { 
             cout<< "Open Spotify... ";
             system("start https://www.spotify.com");
+        }
+        else if (choice == 5) {
+            cout<< "PING WIFI\n";
+            system("ping 8.8.8.8 -n 3");
         }
      
       
